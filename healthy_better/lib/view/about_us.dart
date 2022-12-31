b0955b3a2596c94e9c01ad3a8a95ae76658d26f4
Nyoman Ari S <90953569+nyomanari21@users.noreply.github.com>
2022-12-31 15:12:15 +0700

    Update 2.1
    Tambah frontend about us

--- healthy_better/lib/view/about_us.dart
+++ healthy_better/lib/view/about_us.dart
@@ -16,31 +16,147 @@ class _AboutUsState extends State<AboutUs> {
         title: Text("Settings"),
       ),
       body: Center(
-        child: ListView(
-          padding: EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            // Edit Profile
+            // Team Name
             Container(
-              decoration: BoxDecoration(border: Border(bottom: BorderSide(width: 1, color: Colors.black))),
-              padding: EdgeInsets.all(0),
-              child: ListTile(
-                title: Text("Edit Profile", style: TextStyle(fontSize: 15),),
-                tileColor: Colors.white,
-                onTap: () {
-                  // Tap Function
-                },
+              child: Text("Team Name", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+            ),
+            //
+            Container(
+              width: MediaQuery.of(context).size.width * 0.9,
+              child: Text(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Justo laoreet sit amet cursus sit amet dictum. Accumsan lacus vel facilisis volutpat est velit egestas dui. Orci phasellus egestas tellus rutrum. Lacus suspendisse faucibus interdum posuere lorem ipsum. Fermentum odio eu feugiat pretium nibh ipsum consequat. In fermentum et sollicitudin ac orci. Tellus mauris a diam maecenas sed enim ut. Sit amet est placerat in egestas erat imperdiet sed euismod. Facilisi morbi tempus iaculis urna id volutpat lacus.",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,),
               ),
             ),
-            // My Goals
+            // Our Team
             Container(
-              decoration: BoxDecoration(border: Border(bottom: BorderSide(width: 1, color: Colors.black))),
-              padding: EdgeInsets.all(0),
-              child: ListTile(
-                title: Text("My Goals", style: TextStyle(fontSize: 15),),
-                tileColor: Colors.white,
-                onTap: () {
-                  // Tap Function
-                },
+              child: Column(
+                children: [
+                  Text("Our Team", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        // Team member 1
+                        Container(
+                          margin: EdgeInsets.all(10),
+                          child: Row(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                                child: CircleAvatar(
+                                  backgroundImage: NetworkImage(
+                                    "https://www.pngkey.com/png/full/157-1579943_no-profile-picture-round.png",
+                                  ),
+                                  maxRadius: 30,
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                                child: Column(
+                                  children: [
+                                    Text("Andum Pangestu"),
+                                    Text("Email"),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        // Team 2
+                        Container(
+                          margin: EdgeInsets.all(10),
+                          child: Row(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                                child: CircleAvatar(
+                                  backgroundImage: NetworkImage(
+                                    "https://www.pngkey.com/png/full/157-1579943_no-profile-picture-round.png",
+                                  ),
+                                  maxRadius: 30,
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                                child: Column(
+                                  children: [
+                                    Text("Irfan Mochamad Esa"),
+                                    Text("Email"),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        // Team 3
+                        Container(
+                          margin: EdgeInsets.all(10),
+                          child: Row(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                                child: CircleAvatar(
+                                  backgroundImage: NetworkImage(
+                                    "https://www.pngkey.com/png/full/157-1579943_no-profile-picture-round.png",
+                                  ),
+                                  maxRadius: 30,
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                                child: Column(
+                                  children: [
+                                    Text("Muhammad Rafli Syaputra"),
+                                    Text("Email"),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        // Team 4
+                        Container(
+                          margin: EdgeInsets.all(10),
+                          child: Row(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                                child: CircleAvatar(
+                                  backgroundImage: NetworkImage(
+                                    "https://www.pngkey.com/png/full/157-1579943_no-profile-picture-round.png",
+                                  ),
+                                  maxRadius: 30,
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                                child: Column(
+                                  children: [
+                                    Text("Nyoman Ari Satyadharma"),
+                                    Text("Email"),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
